63c62376285e0390fa7fee66e89ff410836ef757
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-10-18 14:27:54 +0900

    [add] 複数動画に対応

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -12,31 +12,41 @@ option = {
         'format': 'bestvideo+bestaudio/best ,video.mp4'
     }
 
+# url変数,Input変数の宣言
+url = []
+Input = ""
+
 # YouTubeの動画のURLを入力
-URL = input("Enter URL : ")
+while True:
+    Input = input("Enter URL or Enter 0 to run : ")
+    if Input != "0":
+        break
+    url.append(Input)
 
-# yt-dlpで動画のメタデータを取得(動画のタイトル取得のため)
-with YoutubeDL() as ydl:
-    res = ydl.extract_info(URL, download=False)
 
-# 動画のダウンロード
-YoutubeDL(option).download([URL])
+for URL in url:
+    # yt-dlpで動画のメタデータを取得(動画のタイトル取得のため)
+    with YoutubeDL() as ydl:
+        res = ydl.extract_info(URL, download=False)
 
-# 入出力パスを指定
-input_path = os.path.join(Path, "tmpvideo.webm")
-output_path = os.path.join(Path, "convertedvideo.mp4")
+    # 動画のダウンロード
+    YoutubeDL(option).download([URL])
 
-# ffmpegを使用してwebmからmp4へ変換
-input_file = ffmpeg.input(input_path)
-output_file = ffmpeg.output(input_file, output_path)
+    # 入出力パスを指定
+    input_path = os.path.join(Path, "tmpvideo.webm")
+    output_path = os.path.join(Path, "convertedvideo.mp4")
 
-ffmpeg.run(output_file, overwrite_output=True)
+    # ffmpegを使用してwebmからmp4へ変換
+    input_file = ffmpeg.input(input_path)
+    output_file = ffmpeg.output(input_file, output_path)
 
-# 入力ファイルを削除
-os.remove(input_path)
+    ffmpeg.run(output_file, overwrite_output=True)
 
-# 動画ファイルの名前を変更
-os.rename(Path + "/convertedvideo.mp4", Path + "/" + re.sub(r'[/|]', '-', res["title"]) + ".mp4")
-# Windowsの場合、re.sub（）のパターンを'[\\|/|:|?|.|"|<|>|\|]'に変更したら正常に動作する
+    # 入力ファイルを削除
+    os.remove(input_path)
 
-print("Video Download and conversion completed")
+    # 動画ファイルの名前を変更
+    os.rename(Path + "/convertedvideo.mp4", Path + "/" + re.sub(r'[/|]', '-', res["title"]) + ".mp4")
+    # Windowsの場合、re.sub（）のパターンを'[\\|/|:|?|.|"|<|>|\|]'に変更したら正常に動作する
+
+    print("Video Download and conversion completed")
